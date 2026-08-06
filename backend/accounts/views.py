b341df9f25d432fdaca7726c01fa76313eec6f4c
Rajesh Jeelakarra <career.rajesh.jeelakarra@gmail.com>
2026-08-07 00:09:20 +0530

    all files

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import User
 
 def register(request):
-    return render(request,"accounts/register.html")
+
+    if request.method == "POST":
+        print("POST Request Received")
+
+        fullname = request.POST.get("fullname")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+
+        if password == confirm_password:
+            user = User.objects.create_user(
+                username=email,
+                first_name=fullname,
+                email=email,
+                password=password,
+                role=User.CUSTOMER
+            )
+
+            print("User Registered Successfully")
+
+        else:
+            print("Passwords do not match")
+
+    return render(request, "accounts/register.html")
